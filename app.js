2a213b59461126482a4b84e91151c2f2f4ccdaca
UmmuZurak <51839140+UmmuZurak@users.noreply.github.com>
2026-07-21 13:06:13 +0000

    update swagger documentation

--- app.js
+++ app.js
@@ -15,12 +15,10 @@ app.use(bodyParser.json());
 const apiRouter = require("./api");
 const transactionsRouter = require("./routes/transactions");
 
-// generate docs with swagger ui
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('/openapi.yaml');
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+// import swagger ui packages 
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./openapi.yaml");
 
 // get request to send "Hello World" message to http://localhost:3000/
 app.get("/", (req, res, next) => {
@@ -32,6 +30,10 @@ app.use("/api/envelopes", apiRouter);
 
 // transaction routes
 app.use("/api/transactions", transactionsRouter);
+
+// swagger api documentation route
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 module.exports = {
   app,
